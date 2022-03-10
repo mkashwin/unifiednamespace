14b4911f59f12bfb8e86d85a1417c171d533d263
Ashwin Krishnan <mkashwin@gmail.com>
2022-03-10 23:43:18 +0100

    first draft of graph db code

--- 03_uns_graphdb/graphdb_handler.py
+++ 03_uns_graphdb/graphdb_handler.py
@@ -1,38 +1,114 @@
-from neo4j import GraphDatabase
 from config import settings
+import neo4j
 import json
 import logging
 
 LOGGER = logging.getLogger(__name__)
+_NODE_TYPES = settings.graphdb["node_types"]
 
-class UNSCurrentView:
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+class GraphDBHandler:
+
+    def __init__(self, uri : str, user  : str, password : str):
+        ## TODO support other authentications like cert based authentication
+        try:
+            self.driver = neo4j.GraphDatabase.driver(uri, auth=(user, password))
+        except Exception as e:
+            LOGGER.error("Failed to create the driver:", e)
 
     def close(self):
-        self.driver.close()
+        if self.driver is not None:
+            self.driver.close()
 
-    # TODO:  Itereage teh topics by '/'. create node for each level and merge the messages to the final node
-    def persistMQTTmsg(topic, message):
-        # TODO Error handling
-        node_type = ["ENTERPRISE", "FACILITY", "AREA", "LINE", "DEVICE"]
+    def persistMQTTmsg(self, topic: str, message: str):
+        """
+        Iterate the topics by '/'. create node for each level and merge the messages to the final node
+        For the other topics in the hierarchy a node will be created / merged and linked to the parent topic node
+        Parameters
+        ----------
+        topic: str
+            The topic on which the message was sent
+        message: str 
+            The MQTT message. String is expected to be a JSON
+        """
         nodes = topic.split('/')
         count = 0
-        lastnode 
-        for node in nodes:
-            # TODO Logger
-            print(node)
+        lastnode = None
+        with self.driver.session() as session:
+            try:
+                for node in nodes:
+                    # TODO improve logger message
+                    LOGGER.debug(node)
+                    response = None
+                    jsonStr = None
+                    if (count == int(nodes.length)):
+                        ## Save the message only for the leaf node
+                        jsonStr = message
 
-            # refer https://stackoverflow.com/questions/35255540/neo4j-add-update-properties-if-node-exists
-            # TODO create create or merge node in database.
-            # save the last node name to ensure that we are able to insert / merge attributes of the message into that node
-            ## MERGE (node)
-            if(count == int(nodes.length)):
-                attributes = json.loads(message)
-                # FIXME node_type length must be >= nodes.length. if not decide how to handle
-                ## MERGE (node:node_type[count],attributes)
-                print(attributes)
-                for attribute in attributes:
-                    # TODO create create or merge attributes in the last note
-                    print(attribute)
-            lastnode = node
+                    if (count < _NODE_TYPES.length):
+                        response = GraphDBHandler._saveNode(session, node,
+                                                  _NODE_TYPES[count], jsonStr)
+                    else:
+                        response = GraphDBHandler._saveNode(
+                            session, node,
+                            f"{_NODE_TYPES[_NODE_TYPES.length]}_count", jsonStr)
+                    count += 1
+                    lastnode = node
+            except Exception as ex:
+                LOGGER.error(ex)
+            finally:
+                if session is not None:
+                    session.close()
+            return response
+
+    @staticmethod
+    def _saveNode(session : neo4j.Session, nodename: str, nodetype: str, message=None):
+        """
+        Creates or Merges the MQTT message as a Graph node. Each level of the topic is also persisted 
+        as a graph node with appropriate parent relationship
+        Parameters
+        ----------
+        session 
+            Neo4j session object
+        nodename : str
+            Trimmed name of the topic
+        nodetype : str
+            Based on ISA-95 part 2 the nested depth of the topic determines the node type. 
+            Also see settings.yaml graphdb.node_types
+        @message  The JSON string delivered as message in the MQTT payload. Defaults to null
+        """
+        attributes = r'{ }'
+        LOGGER.debug(
+            f"Saving node:{nodename} of type:{nodetype} and message:{message}")
+        if (message is not None):
+            ## FIXME: Neo4j supports only flat messages. also the json.load does not remove quotes around the keys of the dict
+            ## Also need to ensure that the message doesn't contain any attribute with the name "node_name"
+            attributes = GraphDBHandler._flatten_json_for_Neo4J(message)
+        return session.run("MERGE (node:$nodetype { node_name: $nodename }) "
+                           "SET node += $attributes"
+                           "RETURN node",nodetype=nodetype)
+
+    @staticmethod
+    def _flatten_json_for_Neo4J(message) -> str:
+        LOGGER.debug(message)
+        output={}
+        
+        def flatten(json_object, name=''):
+            if type(json_object) is dict:
+                for items in json_object:
+                    flatten(json_object[items], name + items + '_')
+            elif type(json_object) is list:
+                i = 0
+                for items in json_object:
+                    flatten(items, name + str(i) + '_')
+                    i += 1
+            else:
+                output[name[:-1]] = json_object
+
+        flatten(message)
+        string_output = "{"
+        for key, value in output.items():
+            LOGGER.debug(key,value)
+            string_output += f" {key} : \"{value}\" ,"
+        string_output =string_output[:-1] + " }"
+        return string_output
--- 03_uns_graphdb/mqtt_listner.py
+++ 03_uns_graphdb/mqtt_listner.py
@@ -3,18 +3,24 @@ import logging
 
 import paho.mqtt.client as mqtt_client
 from config import settings
+from graphdb_handler import GraphDBHandler
 
 
 # read these values from the configuration file
 mqtt_broker = settings.mqtt["broker_url"]
-mqtt_port = settings.mqtt["port"]
 topic = settings.mqtt["topic"]
 qos = settings.mqtt["qos"]
 keep_alive = settings.mqtt["keep_alive"]
 # generate client ID with pub prefix randomly
 client_id = f'graphdb-{random.randint(0, 1000)}'
-username = settings.mqtt["username"]
-password = settings.mqtt["password"]
+mqtt_username = settings.mqtt["username"]
+mqtt_password = settings.mqtt["password"]
+
+
+graphdb_url = settings.graphdb["url"]
+graphdb_user = settings.graphdb["username"]
+
+graphdb_password = settings.graphdb["password"]
 
 LOGGER = logging.getLogger(__name__)
 
@@ -31,7 +37,7 @@ def on_connect(client, userdata, flags, rc):
     # subscribe to the topic only if connection was successful
     graphdb_client.subscribe(topic, qos)
     # TODO Error handling
-    LOGGER.info( f"Successfully connect {graphdb_client} to MQTT Broker at {mqtt_broker}:{mqtt_port}")
+    LOGGER.info( f"Successfully connect {graphdb_client} to MQTT Broker at {mqtt_broker}")
 
 
 def on_subscribe(client: mqtt_client):
@@ -46,13 +52,20 @@ def on_message(client, userdata, msg):
         f"Message: {msg},"
         "}"
     )
-    ##Conntect to the database
-    ## parse message and topic 
-    ## save message
-    ## disconnect from db
+    ## Connect to the database
+    try :
+        graph_db_handler = GraphDBHandler(graphdb_url,graphdb_user,graphdb_password)
+        ## parse message and topic 
+        ## save message
+        graph_db_handler.persistMQTTmsg(msg.topic,msg)
+        ## disconnect from db
+        graph_db_handler.close()
+    except Exception as ex:
+        LOGGER.error(ex)
 
 
-# create historian mqtt client
+
+# create graphdb mqtt client
 graphdb_client = mqtt_client.Client()
 
 # Assign event callbacks
@@ -61,7 +74,7 @@ graphdb_client.on_subscribe = on_subscribe
 graphdb_client.on_message = on_message
 
 # Connect
-graphdb_client.connect(mqtt_broker, int(mqtt_port), int(keep_alive))
+graphdb_client.connect(mqtt_broker, int(keep_alive))
 
 # Continue the network loop
 graphdb_client.loop_forever()
