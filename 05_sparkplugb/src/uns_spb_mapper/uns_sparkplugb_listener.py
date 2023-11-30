9bd1e20b57ee124ed5b2d325463d0627c9906706
Ashwin Krishnan <mkashwin@gmail.com>
2023-11-30 15:25:47 +0000

    moved config mgmt logic to settings

--- 05_sparkplugb/src/uns_spb_mapper/uns_sparkplugb_listener.py
+++ 05_sparkplugb/src/uns_spb_mapper/uns_sparkplugb_listener.py
@@ -4,11 +4,10 @@ MQTT listener that listens to SparkplugB name space for messages and publishes t
 import logging
 import random
 import time
-from typing import Optional
 
 from uns_mqtt.mqtt_listener import UnsMQTTClient
 
-from uns_spb_mapper.sparkplugb_enc_config import settings
+from uns_spb_mapper.sparkplugb_enc_config import MQTTConfig
 from uns_spb_mapper.spb2unspublisher import Spb2UNSPublisher
 
 LOGGER = logging.getLogger(__name__)
@@ -27,67 +26,30 @@ class UNSSparkPlugBMapper:
         Constructor
         """
         self.uns_client: UnsMQTTClient = None
-        self.load_mqtt_configs()
-        self.load_sparkplugb_configs()
+
+        self.client_id = f"uns_sparkplugb_listener-{time.time()}-{random.randint(0, 1000)}"  # noqa: S311
+
         self.uns_client: UnsMQTTClient = UnsMQTTClient(
             client_id=self.client_id,
-            clean_session=self.clean_session,
+            clean_session=MQTTConfig.clean_session,
             userdata=None,
-            protocol=self.mqtt_mqtt_version_code,
-            transport=self.mqtt_transport,
-            reconnect_on_failure=self.reconnect_on_failure)
+            protocol=MQTTConfig.mqtt_mqtt_version_code,
+            transport=MQTTConfig.mqtt_transport,
+            reconnect_on_failure=MQTTConfig.reconnect_on_failure)
 
         self.uns_client.on_message = self.on_message
         self.uns_client.on_disconnect = self.on_disconnect
 
         self.spb_2_uns_pub: Spb2UNSPublisher = Spb2UNSPublisher(
             self.uns_client)
-        self.uns_client.run(host=self.mqtt_host,
-                            port=self.mqtt_port,
-                            username=self.mqtt_username,
-                            password=self.mqtt_password,
-                            tls=self.mqtt_tls,
-                            keepalive=self.mqtt_keepalive,
-                            topics=self.topics,
-                            qos=self.mqtt_qos)
-
-    def load_mqtt_configs(self):
-        """
-        Read the MQTT configurations required to connect to the MQTT broker
-        """
-        # generate client ID with pub prefix randomly
-        self.client_id = f"uns_sparkplugb_listener-{time.time()}-{random.randint(0, 1000)}"  # noqa: S311
-
-        self.mqtt_transport: Optional[str] = settings.get(
-            "mqtt.transport", "tcp")
-        self.mqtt_mqtt_version_code: int = settings.get(
-            "mqtt.version", UnsMQTTClient.MQTTv5)
-        self.mqtt_qos: int = settings.get("mqtt.qos", 2)
-        self.reconnect_on_failure: bool = settings.get(
-            "mqtt.reconnect_on_failure", True)
-        self.clean_session: bool = settings.get("mqtt.clean_session", None)
-
-        self.mqtt_host: Optional[str] = settings.mqtt["host"]
-        self.mqtt_port: int = settings.get("mqtt.port", 1883)
-        self.mqtt_username: Optional[str] = settings.get("mqtt.username")
-        self.mqtt_password: Optional[str] = settings.get("mqtt.password")
-        self.mqtt_tls: dict = settings.get("mqtt.tls", None)
-        self.topics: list = settings.get("mqtt.topics", ["spBv1.0/#"])
-        self.mqtt_keepalive: int = settings.get("mqtt.keep_alive", 60)
-        self.mqtt_ignored_attributes: dict = settings.get(
-            "mqtt.ignored_attributes", None)
-        self.mqtt_timestamp_key = settings.get("mqtt.timestamp_attribute",
-                                               "timestamp")
-        if self.mqtt_host is None:
-            raise SystemError(
-                "MQTT Host not provided. Update key 'mqtt.host' in '../../conf/settings.yaml'",
-            )
-
-    def load_sparkplugb_configs(self):
-        """
-        Load the SparkplugB configurations
-        """
-        # Currently no configurations
+        self.uns_client.run(host=MQTTConfig.mqtt_host,
+                            port=MQTTConfig.mqtt_port,
+                            username=MQTTConfig.mqtt_username,
+                            password=MQTTConfig.mqtt_password,
+                            tls=MQTTConfig.mqtt_tls,
+                            keepalive=MQTTConfig.mqtt_keepalive,
+                            topics=MQTTConfig.topics,
+                            qos=MQTTConfig.mqtt_qos)
 
     def on_message(self, client, userdata, msg):
         """
