6e7eee2f458d4eb6c60f9c09ef3ca24d6df006a9
Ashwin Krishnan <36015265+mkashwin@users.noreply.github.com>
2024-05-30 09:49:34 +0000

    removed pytest fixture marker deprecation

--- 07_uns_graphql/test/subscriptions/test_kafka.py
+++ 07_uns_graphql/test/subscriptions/test_kafka.py
@@ -144,7 +144,7 @@ async def test_get_kafka_messages_mock(topics: list[KAFKATopicInput], message_va
 @pytest.mark.integrationtest()
 # FIXME not working with VsCode https://github.com/microsoft/vscode-python/issues/19374
 # Comment this marker and run test individually in VSCode. Uncomment for running from command line / CI
-# @pytest.mark.xdist_group(name="graphql_kafka")
+@pytest.mark.xdist_group(name="graphql_kafka")
 @pytest.mark.parametrize(
     "kafka_topics, message_vals",
     [
